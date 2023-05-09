5bcc99a1bc749fe8d0411162034122401b5996c8
Edwin Boon <edwin.boon@eo.nl>
2023-05-09 15:12:51 +0200

    Created default webapp structure and added the getCharacters query

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+const query = gql`
+  query getCharacters {
+    characters {
+      results {
+        id
+        name
+        image
+        status
+        species
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const { data } = await useAsyncQuery(query);
+</script>
+<template>
+  <div class="">{{ data }}</div>
+</template>
