d41d4d0965b9f8b98fe4efc3e8180b14ed7db135
Edwin Boon <edwin.boon@eo.nl>
2023-05-10 09:43:17 +0200

    Created new getCharacter query and charachter detail page

---
+++ pages/character/[id].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import GET_CHARACTER from '@/graphql/queries/getCharacter.gql';
+import type { CharactersResult } from '~/types/Character';
+
+const route = useRoute();
+
+const { data, pending, error } = await useAsyncQuery<CharactersResult>(
+  GET_CHARACTER,
+  { id: route.params.id }
+);
+</script>
+
+<template>
+  <div>{{ route.params.id }}</div>
+  <div>{{ data }}</div>
+</template>
