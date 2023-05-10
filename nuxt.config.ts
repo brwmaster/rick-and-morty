869b565bbcaaaca309a7a5cc90147ae547c5cb72
Edwin Boon <edwin.boon@eo.nl>
2023-05-10 11:53:44 +0200

    Add meta data and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/apollo', '@nuxtjs/tailwindcss', '@nuxt/image-edge'],
-
+  app: {
+    head: {
+      title: 'Rick and Morty',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/favicon.png'
+        }
+      ]
+    }
+  },
   apollo: {
     clients: {
       default: {
